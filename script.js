72b6031dd58fb080f4720e44aa79bd94e66ab4d0
Sardor <sardorismoilov2005@gmail.com>
2025-05-09 19:11:35 +0900

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
+const formBtn = itemForm.querySelector("button");
+let isEditMode = false;
 
-function addItem(e){
+
+function displayToScreen(){
+  const itemsFromStorage = getItemsFromStorage();
+
+  itemsFromStorage.forEach((item) => addItemToDom(item));
+  checkUI();
+}
+
+
+function  onAddItemSubmit(e){
     e.preventDefault();
    const newItem = itemInput.value;
 
@@ -10,17 +23,45 @@ function addItem(e){
     alert("Please add an item!")
     return;
    }
-   
-   const li = document.createElement("li");
-   li.appendChild(document.createTextNode(newItem));
+   //create item dom element
+   addItemToDom(newItem);
+
+   // create item to local storage
+   addToLocalStorage(newItem);   
+   checkUI();
+   itemInput.value = "";
+}
+ 
+
+function addItemToDom(item){
+    const li = document.createElement("li");
+   li.appendChild(document.createTextNode(item));
 
    const button = createButton("remove-item btn-link text-red");  
    li.appendChild(button);
-   
    itemList.appendChild(li);
 
-   itemInput.value = "";
 }
+
+
+function addToLocalStorage(item){
+   const itemsFromStorage = getItemsFromStorage();
+   
+   itemsFromStorage.push(item);
+   localStorage.setItem("items", JSON.stringify(itemsFromStorage));
+}
+
+function getItemsFromStorage(item){
+    let itemsFromStorage;
+
+   if(localStorage.getItem("items") === null){
+    itemsFromStorage = [];
+   }else{
+    itemsFromStorage = JSON.parse(localStorage.getItem("items"));
+   }
+   return itemsFromStorage;
+}
+
 
 function createButton(classes){
     const button = document.createElement("button");
@@ -30,10 +71,100 @@ function createButton(classes){
     return button;
 }
 
+
 function createIcon(classes){
     const icon = document.createElement("i");
     icon.className = classes;
     return icon;
 }
+
+
+function onClickItem(e){
+    if(e.target.parentElement.classList.contains("remove-item")){
+       removeItem(e.target.parentElement.parentElement);
+    }else{
+        setItemToEdit(e.target);
+    }
+}
+
+function setItemToEdit(item) {
+    isEditMode = true;
+
+    itemList.querySelectorAll("li").forEach((i) => i.classList.remove("edit-mode"));
+
+    item.classList.add("edit-mode");
+    formBtn.innerHTML ='<i class="fa-solid fa-pen"></i>   Update Item';
+    formBtn.style.backgroundColor = "#228B22"
+    itemInput.value = item.textContent;
+    
+
+}
  
-itemForm.addEventListener("submit", addItem);
+function removeItem(item) {
+   if(confirm("Are you sure?")){
+    item.remove();
+
+    /// remove from storage
+    removeItemFromStorage(item.textContent);
+    checkUI();
+   }
+}
+
+function  removeItemFromStorage(item){
+    let itemsFromStorage = getItemsFromStorage();
+
+    itemsFromStorage = itemsFromStorage.filter((i)=>  i !== item);
+
+    //resetlocal storage
+    localStorage.setItem("items", JSON.stringify(itemsFromStorage));
+}
+
+
+function clearItems() {
+    // if(confirm("Are you sure?")){
+    //     itemList.innerHTML="";
+    // }
+        while (itemList.firstChild) {
+          itemList.removeChild(itemList.firstChild);
+        }
+        //local storage
+        localStorage.removeItem("items");
+      checkUI();
+}
+
+
+function checkUI() {
+    const items = itemList.querySelectorAll("li");
+    if(items.length === 0) {
+        clearBtn.style.display = "none";
+        itemFilter.style.display = "none";
+    } else{
+        clearBtn.style.display = "block";
+        itemFilter.style.display = "block";
+        
+    }
+}
+
+
+function filterItems(e) {
+    const items = itemList.querySelectorAll("li");
+   const text = e.target.value.toLowerCase();
+
+   items.forEach((item) =>{
+    const itemName = item.firstChild.textContent.toLocaleLowerCase();
+
+    if(itemName.indexOf(text) != -1){
+        item.style.display = "flex";
+    }else{
+        item.style.display = "none";
+    }
+   });
+}
+
+
+itemForm.addEventListener("submit", onAddItemSubmit);
+itemList.addEventListener("click", onClickItem);
+clearBtn.addEventListener("click", clearItems);
+itemFilter.addEventListener("input", filterItems)
+document.addEventListener("DOMContentLoaded", displayToScreen);
+checkUI();
